42fcb4be9538252ef4c07b08825cc60de2304138
kankishougo <148510525+kankishougo@users.noreply.github.com>
2023-11-24 08:26:23 +0000

    Added file mount.c

--- mount.c
+++ mount.c
@@ -2,34 +2,45 @@
 #include <stdlib.h>
 #include <sys/mount.h>
 
-#define FILENAMESIZE 100 //ディスクイメージのファイル名の文字数
-
-int main(void)
-{
-    char filename[FILESIZENAME]; //ディスクイメージのファイル名のための文字列
-    FILE *fp;
+int main(void){
+    char fname_R[]; = "sdb1_image.img"; //取得するUSBメモリのイメージファイル名のための文字列
+    char fname_W[]; = "test.txt"; //USBメモリへ書き込むファイル名
+    FILE *fp_r;
+    FILE *fp_w;
     int ret;
     int mnt_flag = 0;
     
-    printf("作成するイメージファイル名を入力してください(拡張子は.img)：");
-    scanf("%s", filename);
+    fp_r = fopen(fname_R,"r"); //USBメモリのイメージファイル取得
     
-    fp = fopen(filename, "w");
-    
-    if(fp == NULL){
-      printf("error\n");
-      exit(1);
+    if(fp_r == NULL){
+      printf("%s file not open\n",fname_R);
+      return -1;
+    }else{
+      printf("%s file opened\n",fname_R);
     }
-    
-    fclose(fp);
 
-    ret = mount("/dev/sdb1", "filename", "vfat", mnt_flag, "codepage=932,iocharset=utf8");
+    ret = mount("/dev/sdb1", "fname", "vfat", mnt_flag, "codepage=932,iocharset=utf8"); //取得イメージのマウント
     
     if (ret == -1) {
         perror("mount:");
         exit(1);
     }
+    
+    fclose(fp_r);
 
     printf("mount succeeded\n");
+        
+    fp_w = fopen(fname_W,"w"); //書き込むファイル取得
+    
+    if(fp_w == NULL){
+      printf("%s file not open.\n",fname_W);
+      return -1;
+    }else{
+      printf("%s file opened.\n",fname_W);
+    }
+    
+    fprintf(fp_w,"executed a write to a file"); //ファイルを書き込み
+    fclose(fp_w);
+
     return 0;
 }
